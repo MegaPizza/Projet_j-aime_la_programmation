76ca1a51ea31108e4ae3b19e51f6b8bacb6c8d84
agathe <mange@10.10.218.196>
2018-05-18 18:12:13 +0200

    premier essais interface, apparition d'une interface vide

--- BPOO test/src/jeu/Interface.java
+++ BPOO test/src/jeu/Interface.java
@@ -1,15 +1,19 @@
 import javax.swing.JFrame;
-
+ 
 public class Test {
+  public static void main(String[] args){
 
-  public static void main(String[] args){       
-
-	//création d'une fenêtre
     JFrame fenetre = new JFrame();
-    
-    //on rend visible cette fenêtre
+                
+    //titre pour notre fenêtre
+    fenetre.setTitle("Ma première fenêtre Java");
+    //Définit sa taille : 400 pixels de large et 400 pixels de haut
+    fenetre.setSize(400, 400);
+    //position au centre
+    fenetre.setLocationRelativeTo(null);
+    //Termine le processus lorsqu'on clique sur la croix rouge
+    fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    //rendre visible        
     fenetre.setVisible(true);
-
   }       
-
 }
